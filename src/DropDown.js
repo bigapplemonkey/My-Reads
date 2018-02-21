1cfa57850d3920f5cd06ecffb2801d8e6aac8ae4
jorge.asuaje@gmail.com <jasuaje@brinqa.com>
2018-02-21 17:39:08 -0500

    Code refactoring/clean-up

--- src/DropDown.js
+++ src/DropDown.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
+/**
+ * click Outside Component:
+ * https://codepen.io/graubnla/pen/EgdgZm
+ */
 class DropDown extends Component {
-  //Handle click outside of React component
-  //https://codepen.io/graubnla/pen/EgdgZm
-  //How to capitalize first letter of each word
-  //https://stackoverflow.com/questions/4878756/how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
   constructor(props) {
     super(props);
 
@@ -13,15 +13,12 @@ class DropDown extends Component {
 
     this.state = {
       isExpanded: false,
-      optionSelected: props.options
-        ? props.options[0]
-        : { value: "All", id: "All" }
+      optionSelected: props.options[0]
     };
   }
 
   handleClick() {
     if (!this.state.isExpanded) {
-      // attach/remove event handler
       document.addEventListener("click", this.handleOutsideClick, false);
     } else {
       document.removeEventListener("click", this.handleOutsideClick, false);
@@ -30,17 +27,6 @@ class DropDown extends Component {
     this.setState(prevState => ({
       isExpanded: !prevState.isExpanded
     }));
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("click", this.handleOutsideClick, false);
-  }
-
-  onSelect(optionSelected) {
-    this.setState({ optionSelected });
-    if (this.props.hasOwnProperty("itemID"))
-      optionSelected["itemID"] = this.props.itemID;
-    this.props.onSelect(optionSelected);
   }
 
   handleOutsideClick(event) {
@@ -52,56 +38,78 @@ class DropDown extends Component {
     this.handleClick();
   }
 
+  componentWillUnmount() {
+    // remove event listener before losing ref in the component
+    document.removeEventListener("click", this.handleOutsideClick, false);
+  }
+
+  onSelect(optionSelected, event) {
+    event.preventDefault();
+
+    this.setState({ optionSelected });
+    // if the component relates to an item which ID could be used
+    if (this.props.hasOwnProperty("itemID"))
+      optionSelected["itemID"] = this.props.itemID;
+
+    this.props.onSelect(optionSelected);
+  }
+
   expand(event) {
     event.preventDefault();
+
     if (!this.state.isExpanded) this.setState({ isExpanded: true });
   }
+
+  correctCapitalization(rawString) {
+    return rawString.replace(
+      /\w\S*/g,
+      text => text.charAt(0).toUpperCase() + text.substr(1).toLowerCase()
+    );
+  }
+
   render() {
     const self = this;
 
+    // filter out duplicated option IDs
     let options = self.props.options.filter(
-      (thing, index, self) =>
-        self.findIndex(t => t.id.toLowerCase() === thing.id.toLowerCase()) ===
+      (option, index, self) =>
+        self.findIndex(t => t.id.toLowerCase() === option.id.toLowerCase()) ===
         index
     );
 
+    // if items are requested to be sorted
     if (self.props.hasOwnProperty("isOrdered") && self.props.isOrdered)
       options = options.sort((option1, option2) => {
-        let textA = option1.value.toUpperCase();
-        let textB = option2.value.toUpperCase();
-        return textA < textB ? -1 : textA > textB ? 1 : 0;
+        let option1Value = option1.value.toUpperCase();
+        let option2Value = option2.value.toUpperCase();
+        return option1Value < option2Value
+          ? -1
+          : option1Value > option2Value ? 1 : 0;
       });
 
+    // Dynamic classes:
+    // menu alignment
+    const alignmentClass = self.props.isRight ? " is-right" : "";
+
+    const expandedClass = self.state.isExpanded ? " is-active" : "";
+
     return (
-      <div
-        className={`my-dropdown dropdown ${
-          self.props.isRight ? "is-right" : ""
-        } ${self.state.isExpanded ? "is-active" : ""}`}
-      >
+      <div className={`my-dropdown dropdown ${alignmentClass}${expandedClass}`}>
         <div
           className="dropdown-trigger"
-          onClick={event => {
-            event.preventDefault();
-            self.handleClick(event);
-          }}
+          onClick={event => self.handleClick(event)}
           ref={node => (self.node = node)}
         >
           <button
             className="button"
             aria-haspopup="true"
-            aria-controls="dropdown-menu3"
+            aria-controls="dropdown-menu"
           >
             {self.props.onlyArrow ? (
               ""
             ) : (
               <span>
-                {self.state.optionSelected.value.replace(/\w\S*/g, function(
-                  txt
-                ) {
-                  return (
-                    txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-                  );
-                })}
+                {self.correctCapitalization(self.state.optionSelected.value)}
               </span>
             )}
             <span className="icon is-small">
@@ -114,11 +122,7 @@ class DropDown extends Component {
             {options.map(option => {
               return option.isDisabled ? (
                 <p key={option.id} className="dropdown-item disabled">
-                  {option.value.replace(/\w\S*/g, function(txt) {
-                    return (
-                      txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-                    );
-                  })}
+                  {self.correctCapitalization(option.value)}
                   <span className="icon is-small dropdown-icon">
                     <i className="fas fa-check" />
                   </span>
@@ -128,16 +132,9 @@ class DropDown extends Component {
                   href=""
                   key={option.id}
                   className="dropdown-item"
-                  onClick={event => {
-                    event.preventDefault();
-                    self.onSelect(option, event);
-                  }}
+                  onClick={event => self.onSelect(option, event)}
                 >
-                  {option.value.replace(/\w\S*/g, function(txt) {
-                    return (
-                      txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-                    );
-                  })}
+                  {self.correctCapitalization(option.value)}
                 </a>
               );
             })}
--- src/Rating.js
+++ src/Rating.js
@@ -1,45 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Rating extends Component {
-  //Start rating
-  //https://codepen.io/christianhegedues/pen/OxyOBb/
-  render() {
-    const self = this;
-
-    return (
-      <div>
-        <div className="star-rating">
-          <span>☆</span>
-          <span>☆</span>
-          <span>☆</span>
-          <span>☆</span>
-          <span>☆</span>
-          <div
-            className="star-rating__current"
-            style={{
-              width: self.props.averageRating
-                ? `${(self.props.averageRating / 5 * 100).toFixed(2)}%`
-                : 0
-            }}
-          >
-            <span>★</span>
-            <span>★</span>
-            <span>★</span>
-            <span>★</span>
-            <span>★</span>
-          </div>
-        </div>
-        <div className="reviewers-count has-text-info has-text-centered">
-          <p className="reviewer-count is-size-7">
-            {self.props.ratingsCount ? self.props.ratingsCount : 0}
-          </p>
-          <div className="icon reviewer-icon">
-            <i className="fas fa-users" />
-          </div>
-        </div>
+/**
+ * Start Rating:
+ * https://codepen.io/christianhegedues/pen/OxyOBb/
+ */
+const Rating = props => (
+  <div>
+    <div className="star-rating">
+      <span>☆</span>
+      <span>☆</span>
+      <span>☆</span>
+      <span>☆</span>
+      <span>☆</span>
+      <div
+        className="star-rating__current"
+        style={{
+          width: props.averageRating
+            ? `${(props.averageRating / 5 * 100).toFixed(2)}%`
+            : 0
+        }}
+      >
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
       </div>
-    );
-  }
-}
+    </div>
+    <div className="reviewers-count has-text-info has-text-centered">
+      <p className="reviewer-count is-size-7">
+        {props.ratingsCount ? props.ratingsCount : 0}
+      </p>
+      <div className="icon reviewer-icon">
+        <i className="fas fa-users" />
+      </div>
+    </div>
+  </div>
+);
 
 export default Rating;

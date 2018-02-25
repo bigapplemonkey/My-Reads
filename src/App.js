6bc2b0d01cec2682701e48beec8f341076415fe3
jorge.asuaje@gmail.com <jasuaje@brinqa.com>
2018-02-25 16:00:28 -0500

    Code refactoring/cleaning-up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
+// React packages
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import * as BooksAPI from "./BooksAPI";
-import Header from "./Header";
-import Footer from "./Footer";
-import CategoryList from "./CategoryList";
-import Modal from "./Modal";
-import "./App.css";
-import "./Custom.css";
+// Book API
+import * as BooksAPI from './BooksAPI';
+// Components
+import Header from './Header';
+import Footer from './Footer';
+import CategoryList from './CategoryList';
+import Modal from './Modal';
+// Styling
+import './App.css';
+import './Custom.css';
 
 class App extends Component {
   constructor(props) {
@@ -17,12 +21,11 @@ class App extends Component {
       categorizedItems: {},
       categoryCount: {},
       queriedItems: [],
-      selectedTab: this.appConfig.header.menuOptions[0].id,
-      itemOnModalID: "",
+      selectedTab: '',
+      itemOnModal: {},
       isModalVisible: false,
-      updatedTab: "",
+      updatedTab: '',
       isIncreaseUpdate: false,
-      query: "",
       itemIsProcessing: false,
       searchIsProcessing: false
     };
@@ -34,49 +37,34 @@ class App extends Component {
     this.onSearch = this.onSearch.bind(this);
   }
 
+  // app config map
   appConfig = {
     header: {
-      title: "My Reads",
-      subTitle: "A Book Tracking App",
+      title: 'My Reads',
+      subTitle: 'A Book Tracking App',
       menuOptions: [
-        { value: "Currently Reading", id: "currentlyReading" },
-        { value: "Want to Read", id: "wantToRead" },
-        { value: "Read", id: "read" },
-        { value: "Search", id: "search" }
+        { value: 'Currently Reading', id: 'currentlyReading' },
+        { value: 'Want to Read', id: 'wantToRead' },
+        { value: 'Read', id: 'read' },
+        { value: 'Search', id: 'search' }
       ]
     },
     footer: {
-      projectName: "My Reads",
-      authorName: "Jorge Asuaje",
-      authorLink: "https://bigapplemonkey.github.io"
+      projectName: 'My Reads',
+      authorName: 'Jorge Asuaje',
+      authorLink: 'https://bigapplemonkey.github.io'
     }
   };
 
+  // caching the categories present
   categoryValues = this.appConfig.header.menuOptions.filter(
-    category => category.id !== "search"
+    category => category.id !== 'search'
   );
 
-  processedResponse(response) {
-    let allItems = [];
-
-    this.categoryValues.forEach(
-      category =>
-        (allItems = allItems.concat(this.state.categorizedItems[category.id]))
-    );
-    return response.map(responseItem => {
-      const shelfItem = allItems.filter(
-        shelfItem => shelfItem.id === responseItem.id
-      );
-      shelfItem.length > 0
-        ? (responseItem.shelf = shelfItem[0].shelf)
-        : (responseItem.shelf = "none");
-      return responseItem;
-    });
-  }
-
+  // when app mounts
   componentDidMount() {
     this.getAllBooks((categorizedItems, categoryCount) => {
-      // remove the js-loading class so all animations start
+      // set initial items and remove the js-loading class so all animations start
       this.setState(
         {
           categorizedItems: categorizedItems,
@@ -85,46 +73,40 @@ class App extends Component {
         },
         () =>
           (document.body.className = document.body.className.replace(
-            "js-loading",
-            ""
+            'js-loading',
+            ''
           ))
       );
     });
   }
 
+  // get all books when app loads or when an actions happens
   getAllBooks(callback) {
     let categorizedItems = {};
     let categoryCount = {};
 
     BooksAPI.getAll().then(items => {
+      // categorize items
       items.forEach(item => {
         if (!categorizedItems[item.shelf]) categorizedItems[item.shelf] = [];
         categorizedItems[item.shelf].push(item);
       });
 
+      // add empty array for any empty shelf
       this.categoryValues.forEach(category => {
         if (!categorizedItems[category.id]) categorizedItems[category.id] = [];
       });
 
+      // get item count for evey shelf
       Object.keys(categorizedItems).forEach(
         key => (categoryCount[key] = categorizedItems[key].length)
       );
+      // when done getting items
       callback(categorizedItems, categoryCount);
     });
   }
 
-  onShowMoreInfo(itemID) {
-    this.setState({ isModalVisible: true, itemOnModalID: itemID });
-  }
-
-  onHideMoreInfo(itemID) {
-    this.setState({ isModalVisible: false });
-  }
-
-  onCategoryListDisplay(category) {
-    this.setState({ selectedTab: category });
-  }
-
+  // searh
   onSearch(query) {
     this.setState({ searchIsProcessing: true }, () => {
       if (!query || query.length === 0) {
@@ -138,7 +120,7 @@ class App extends Component {
                 () => this.setState({ searchIsProcessing: false })
               );
             } else {
-              console.log("error", response.error);
+              console.log('error', response.error);
               this.setState({ queriedItems: [], searchIsProcessing: false });
             }
           }
@@ -147,24 +129,34 @@ class App extends Component {
     });
   }
 
-  getModalItem() {
-    return this.state.selectedTab === "search"
-      ? this.state.queriedItems.find(
-          item => item.id === this.state.itemOnModalID
-        )
-      : this.state.categorizedItems[this.state.selectedTab].find(
-          item => item.id === this.state.itemOnModalID
-        );
+  // check if the search items are already in shelves
+  processedResponse(response) {
+    let allItems = [];
+
+    this.categoryValues.forEach(
+      category =>
+        (allItems = allItems.concat(this.state.categorizedItems[category.id]))
+    );
+    return response.map(responseItem => {
+      const shelfItem = allItems.filter(
+        shelfItem => shelfItem.id === responseItem.id
+      );
+      shelfItem.length > 0
+        ? (responseItem.shelf = shelfItem[0].shelf)
+        : (responseItem.shelf = 'none');
+      return responseItem;
+    });
   }
 
+  // handle action on item: add, move shelves or remove
   onItemAction(item, moveTo) {
     const moveFrom = item.shelf;
 
     this.setState({ itemIsProcessing: true });
     BooksAPI.update(item, moveTo).then(response => {
       this.getAllBooks((categorizedItems, categoryCount) => {
-        const isIncreaseUpdate = moveTo !== "none";
-        const updatedTab = moveTo !== "none" ? moveTo : moveFrom;
+        const isIncreaseUpdate = moveTo !== 'none';
+        const updatedTab = moveTo !== 'none' ? moveTo : moveFrom;
 
         this.setState(
           {
@@ -183,53 +175,77 @@ class App extends Component {
     });
   }
 
+  // trigger modal for item
+  onShowMoreInfo(item) {
+    this.setState({ isModalVisible: true, itemOnModal: item });
+  }
+
+  // handle notification from modal when closed
+  onHideMoreInfo(item) {
+    this.setState({ isModalVisible: false });
+  }
+
+  // get notification from CategoryList component to update selected tab in header
+  onCategoryListDisplay(category) {
+    this.setState({ selectedTab: category });
+  }
+
   render() {
     const self = this;
 
     // Dynamic classes:
     // app show class
-    const showClass = self.state.isAppReady ? " is-visible" : "";
+    const showClass = self.state.isAppReady ? ' is-visible' : '';
 
     return (
       <div role="application" className={`my-application${showClass}`}>
         <Header
-            config={self.appConfig.header}
-            menuCounts={self.state.categoryCount}
-            selectedTab={self.state.selectedTab}
-            updatedTab={self.state.updatedTab}
-            isIncreaseUpdate={self.state.isIncreaseUpdate}
-            isProcessing={self.state.itemIsProcessing}
-          />
+          config={self.appConfig.header}
+          menuCounts={self.state.categoryCount}
+          selectedTab={self.state.selectedTab}
+          updatedTab={self.state.updatedTab}
+          isIncreaseUpdate={self.state.isIncreaseUpdate}
+          isProcessing={self.state.itemIsProcessing}
+        />
         <main>
           {self.appConfig.header.menuOptions.map((category, index) => (
-            <Route path={ index === 0 ? `${process.env.PUBLIC_URL}/(|${category.id})` :`${process.env.PUBLIC_URL}/${category.id}`} key={category.id} render={()=>(
-              <CategoryList
-              key={category.id}
-              pageTitle={category.value}
-              onDisplay={self.onCategoryListDisplay}
-              items={category.id !== "search" ? self.state.categorizedItems[category.id] : self.state.queriedItems}
-              category={category.id}
-              categoryValues={self.categoryValues}
-              onItemAction={self.onItemAction}
-              onShowMoreInfo={self.onShowMoreInfo}
-              isSearch={category.id === "search"}
-              onSearch={self.onSearch}
-              isProcessing={
-                self.state.searchIsProcessing || self.state.itemIsProcessing
+            <Route
+              path={
+                index === 0
+                  ? `${process.env.PUBLIC_URL}/(|${category.id})`
+                  : `${process.env.PUBLIC_URL}/${category.id}`
               }
+              key={category.id}
+              render={() => (
+                <CategoryList
+                  key={category.id}
+                  pageTitle={category.value}
+                  onDisplay={self.onCategoryListDisplay}
+                  items={
+                    category.id !== "search"
+                      ? self.state.categorizedItems[category.id]
+                      : self.state.queriedItems
+                  }
+                  category={category.id}
+                  categoryValues={self.categoryValues}
+                  onItemAction={self.onItemAction}
+                  onShowMoreInfo={self.onShowMoreInfo}
+                  isSearch={category.id === "search"}
+                  onSearch={self.onSearch}
+                  isProcessing={
+                    self.state.searchIsProcessing || self.state.itemIsProcessing
+                  }
+                />
+              )}
             />
-              )} />
           ))}
         </main>
         <Footer config={self.appConfig.footer} />
-        {self.state.itemOnModalID &&
-          self.state.isModalVisible && (
-            <Modal
-              item={self.getModalItem()}
-              isVisible={self.state.isModalVisible}
-              onHidden={self.onHideMoreInfo}
-            />
-          )}
+        <Modal
+          item={self.state.itemOnModal}
+          isVisible={self.state.isModalVisible}
+          onHidden={self.onHideMoreInfo}
+        />
       </div>
     );
   }
